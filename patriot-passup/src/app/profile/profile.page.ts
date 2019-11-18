f220dcd554dcb308514350e62c7485844f21446d
Gogster7 <gogsterog@gmail.com>
2019-11-17 23:35:53 -0500

    delete post function

--- patriot-passup/src/app/profile/profile.page.ts
+++ patriot-passup/src/app/profile/profile.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 
-import { MenuController, NavController } from '@ionic/angular';
+import { MenuController, NavController, AlertController } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-profile',
@@ -14,10 +15,35 @@ export class ProfilePage  {
 
   userPosts
 
-  constructor(private user: UserService, private afs: AngularFirestore, private afAuth: AngularFireAuth, public navCtr: NavController, public menuCtrl: MenuController) {
+  constructor( private alert: AlertController, private user: UserService, private afs: AngularFirestore, private afAuth: AngularFireAuth, public navCtr: NavController, public menuCtrl: MenuController) {
     this.menuCtrl.enable(true);
     const items= afs.doc(`Users/${user.getUID()}`)
     this.userPosts = items.valueChanges()
+  }
+
+  async showConfirmAlert(item) {
+    const itemDoc= this.afs.doc(`Users/${item}`)
+    const alertConfirm = this.alert.create({
+      header: 'Delete Items',
+      message: 'Have you sold this item?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            console.log('No clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            itemDoc.delete();
+            console.log('Yes clicked');
+          }
+        }
+      ]
+    });
+    (await alertConfirm).present();
   }
 
 
